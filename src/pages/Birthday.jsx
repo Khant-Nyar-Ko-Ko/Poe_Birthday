20863d203acdac12565f4a671b1b8ea5e0843422
khantnyarkoko <kokhantnyar19@gmail.com>
2023-12-25 10:56:26 +0700

    V1_1

--- src/pages/Birthday.jsx
+++ src/pages/Birthday.jsx
@@ -4,11 +4,11 @@ import birthday from "../images/birthday.jpg"
 
 const Birthday = () => {
   return (
-    <div className=" absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[80%]">
+    <div className=" absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[100%]">
         <Link to={'/menu'}>
-      <img src={birthday} className=" rounded" alt="" />
+      <img src={birthday} className=" mx-auto px-5 my-2 w-[500px] rounded-xl" alt="" />
       <h4 className=" text-pink-300 font-serif text-2xl mt-5 text-center select-none">Happy Birthday, Poe</h4>
-      <p className=" text-slate-50 mt-10 select-none">see more</p>
+      <p className=" text-pink-100 mt-5 select-none text-center">see more</p>
         </Link>
     </div>
   );
