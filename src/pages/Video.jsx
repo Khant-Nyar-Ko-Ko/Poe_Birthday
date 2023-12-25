6ca573df8a9db0ad4f64084bcbafecd6812e61b7
khantnyarkoko <kokhantnyar19@gmail.com>
2023-12-25 11:39:10 +0700

    V1_4

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,22 +1,45 @@
+import { useState } from "react";
 import BackToHome from "../components/BackToHome";
 import myVideo from "../video/HappyBd.mp4";
 
 const Video = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleVideoLoad = () => {
+    setIsLoading(false);
+  };
+
   return (
     <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] w-[80%]">
       <div className="max-w-md mx-auto my-4">
-        <h2 className="text-2xl text-center font-semibold text-pink-500 mb-2">
-          Recap
-        </h2>
+        {!isLoading && (
+          <h2 className="text-2xl text-center font-semibold text-pink-500 mb-2">
+            Recap
+          </h2>
+        )}
+
         <div className="relative aspect-w-16 aspect-h-5">
-          <video className=" border-2 border-pink-500 rounded shadow" controls>
+          {isLoading && (
+            <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center text-3xl text-pink-500 bg-opacity-75">
+              Loading...
+            </div>
+          )}
+          <video
+            className={`border-2 border-pink-500 rounded shadow ${
+              isLoading ? "hidden" : ""
+            }`}
+            controls
+            onLoadedData={handleVideoLoad}
+          >
             <source src={myVideo} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
-        <div className=" mt-5">
-          <BackToHome />
-        </div>
+        {!isLoading && (
+          <div className="mt-5">
+            <BackToHome />
+          </div>
+        )}
       </div>
     </div>
   );
